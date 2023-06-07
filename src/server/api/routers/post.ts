e6b13ddc273056f246b0e9eaa17a3b33d3046dae
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-06-07 04:54:48 -0600

    post view and styling

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,8 +1,46 @@
 import { z } from 'zod'
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
+import { clerkClient } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/dist/types/server'
+import { TRPCError } from '@trpc/server'
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    pfp: user.profileImageUrl,
+    emails: user.emailAddresses,
+  }
+}
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany()
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    })
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.userId),
+        limit: 100,
+      })
+    ).map(filterUserForClient)
+
+    return posts.map((post) => {
+      const foundUser = users.find((user) => user.id === post.userId)
+
+      if (!foundUser || !foundUser.username)
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'User not found',
+        })
+
+      return {
+        post,
+        user: { ...foundUser, username: foundUser.username },
+      }
+    })
   }),
 })
